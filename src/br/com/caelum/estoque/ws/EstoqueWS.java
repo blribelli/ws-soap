2afbf59712f2155a860246fea72aef49e1ec9449
blribelli <blribelli@gmail.com>
2017-12-17 23:10:24 -0200

    aulas 4 e 5

--- src/br/com/caelum/estoque/ws/EstoqueWS.java
+++ src/br/com/caelum/estoque/ws/EstoqueWS.java
@@ -6,6 +6,10 @@ import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.ParameterStyle;
+import javax.jws.soap.SOAPBinding.Style;
+import javax.jws.soap.SOAPBinding.Use;
 import javax.xml.ws.RequestWrapper;
 import javax.xml.ws.ResponseWrapper;
 
@@ -19,6 +23,7 @@ import br.com.caelum.estoque.modelo.usuario.TokenDao;
 import br.com.caelum.estoque.modelo.usuario.TokenUsuario;
 
 @WebService
+@SOAPBinding(style=Style.DOCUMENT,  use=Use.LITERAL, parameterStyle=ParameterStyle.WRAPPED)
 public class EstoqueWS {
 	private ItemDao dao = new ItemDao();
 
